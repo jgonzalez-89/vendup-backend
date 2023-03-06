7f8a905103e329befe4d099f85625086fc19d365
jgonzalez-89 <kbcerverjo@gmail.com>
2023-03-06 15:31:23 +0100

    create wsgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,6 +1,8 @@
-from src.app import create_app
+from flask import Flask
+from api.routes import api
 
-app = create_app()
+app = Flask(__name__)
+app.register_blueprint(api, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run()
