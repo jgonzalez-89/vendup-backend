873f44d57eafa570ba2c844977ac9eac8c9b7291
jgonzalez-89 <kbcerverjo@gmail.com>
2023-03-06 15:23:04 +0100

    remove run.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,4 @@
-# This file was created to run the application on heroku using gunicorn.
-# Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
-
-from api import create_app
+from src.app import create_app
 
 app = create_app()
 
